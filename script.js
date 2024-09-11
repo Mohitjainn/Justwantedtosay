1bc7cb4b67bde0505c1945a65854b64d320a907d
Mohitjainn <delipjain123456@gmail.com>
2024-09-11 21:22:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+
+// Change text and gif when the Yes button is clicked
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "🥰🥰🥰I LOVE YOU Ariya cutu 🥰🥰🥰 ";
+  gif.src = "https://media.giphy.com/media/0ivzm1DKtLBCtSSMNH/giphy.gif?cid=ecf05e47tus7v3ty4xxdlngmkbzefq6wcvjioco27ssiatry&ep=v1_gifs_search&rid=giphy.gif&ct=g";
+});
+
+// Make the No button move randomly on hover
+noBtn.addEventListener("mouseover", () => {
+  const wrapper = document.querySelector(".wrapper");
+  const wrapperRect = wrapper.getBoundingClientRect();
+  const noBtnRect = noBtn.getBoundingClientRect();
+
+  // Calculate max positions to ensure the button stays within the wrapper
+  const maxX = wrapperRect.width - noBtnRect.width;
+  const maxY = wrapperRect.height - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
 
